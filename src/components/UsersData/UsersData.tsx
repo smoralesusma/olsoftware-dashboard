3ebc143ab2d89186c5f0e0b1187faa0cdcea1f2e
Sergio Morales <s.moralesusma@gmail.com>
2020-08-01 04:47:37 -0500

    Users Data + Users Table compoenents

---
+++ src/components/UsersData/UsersData.tsx
@@ -0,0 +1,299 @@
+import React, { useState, useEffect, useContext } from "react";
+import firebase from "firebase";
+import {
+  Box,
+  Grid,
+  Paper,
+  Typography,
+  FormControl,
+  InputLabel, 
+  OutlinedInput,
+  Select,
+  MenuItem,
+  Button,
+  makeStyles,
+  useTheme } from "@material-ui/core";
+import { Group, GroupAdd } from "@material-ui/icons";
+
+import UsersTable from "../UsersData/UsersTable";
+import CreateUserButton from "./CreateUserButton";
+import { AuthContext } from "../Tools/Auth";
+import firebaseInitApp from "../../services/Firebase";
+
+export interface IUserData {
+  id?: string;
+  names: string;
+  lastnames: string;
+  identification: number;
+  rol: "administrador" | "conductor" | "recolector";
+  state: boolean;
+  phone: number;
+  email: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    overflow: "auto"
+  },
+  titleTable: {
+    display: 'flex',
+    alignItems: "center",
+    flexGrow: 1,
+    paddingBottom: theme.spacing(2)
+  },
+  titleIcon: {
+    fontSize: 30,
+    paddingRight: theme.spacing(2)
+  },
+  titleText: {
+    [theme.breakpoints.down('sm')]: {
+      display: "none"
+    },
+  }
+}))
+
+const UsersData = () => {
+  const [users, setUsers] = useState<IUserData[] | undefined>(undefined);
+  const [filteredUsers, setFilteredUsers] = useState<IUserData[] | undefined>(undefined);
+  const [canEdit, setCanEdit] = useState<boolean>(false);
+  const { currentUser } = useContext(AuthContext);
+
+  const theme = useTheme();
+  const classes = useStyles(theme);
+
+  const onFilterUsers  = (event: any) => {
+    event.preventDefault();
+    const {
+      names,
+      lastnames,
+      identification,
+      rol,
+      state,
+      phone,
+      email } = event.target.elements;
+
+    const namesValue: string = names.value.trim();
+    const lastnamesValue: string = lastnames.value.trim();
+    const identificationValue: string = identification.value.trim();
+    const rolValue: string = rol.value.trim();
+    const stateValue: string = state.value.trim();
+    const phoneValue: string = phone.value.trim();
+    const emailValue: string = email.value.trim();
+    
+    if (users) {
+      let newFilter = users;
+      if (namesValue.length > 0) newFilter = newFilter.filter((user) => user.names.includes(namesValue));
+      if (lastnamesValue.length > 0) newFilter = newFilter.filter((user) => user.lastnames.includes(lastnamesValue));
+      if (identificationValue.length > 0) newFilter = newFilter.filter((user) => user.identification.toString().includes(identificationValue));
+      if (rolValue.length > 0) newFilter = newFilter.filter((user) => user.rol === rolValue);
+      if (stateValue.length > 0) newFilter = newFilter.filter((user) => {
+        if (user.state) return '1' === stateValue;
+        else return '0' === stateValue;
+      });
+      if (phoneValue.length > 0) newFilter = newFilter.filter((user) => user.phone.toString().includes(phoneValue));
+      if (emailValue.length > 0) newFilter = newFilter.filter((user) => user.email.includes(emailValue));
+
+      if (newFilter.length > 0) setFilteredUsers([...newFilter]);
+      else setFilteredUsers(undefined)
+    }
+  }
+
+  useEffect(() => {
+    if (currentUser) {
+      const usersDB = firebase.firestore(firebaseInitApp).collection("USER");
+      usersDB.get().then(async (users) => {
+        const usersData: IUserData[] = [];
+        let isNewUser = true;
+        users.docs.forEach(user => {
+          const userData = user.data();
+          if (userData.email === currentUser.email) {
+            setCanEdit(userData.rol === "administrador");
+            isNewUser = false;
+          }
+          const userInfo: IUserData = {
+            id: user.id,
+            ...userData as any,
+          }
+          usersData.push(userInfo);
+        });
+        if (isNewUser) {
+          const newUserData: IUserData = {
+            names: "N/A",
+            lastnames: "N/A",
+            identification: 0,
+            rol: "conductor",
+            state: false,
+            phone: 1234567,
+            email: currentUser.email!
+          }
+          const newUser = await usersDB.add(newUserData);
+
+          newUserData.id = newUser.id;
+
+          usersData.push(newUserData);
+          setUsers([...usersData]);          
+        } else setUsers([...usersData]);
+      })
+    }
+  }, [currentUser]);
+  return <Grid container spacing={2}>
+    <Grid item xs={12} lg={9}>
+      <Paper className={classes.paper}>
+        <Box className={classes.titleTable}>
+          <Box
+            px={2}
+            display="flex"
+            alignItems="center"
+            flexGrow={1}>
+            <Group color="primary" className={classes.titleIcon}/>
+            <Typography
+              className={classes.titleText}
+              variant="h6"
+              color="primary"
+              component="p">Usuarios existentes
+            </Typography>
+          </Box>
+          <CreateUserButton
+            setUser={(newUserData) => {
+              const newUsers = users;
+              if (newUsers) {
+                newUsers.push(newUserData);
+                setUsers([...newUsers])
+              } else setUsers([newUserData]);
+            }}/>
+        </Box>
+        <UsersTable
+          users={users}
+          canEdit={canEdit}
+          setUsers={setUsers}
+          filteredUsers={filteredUsers}/>
+      </Paper>
+    </Grid>
+    <Grid item xs={12} lg={3}>
+      <Paper className={classes.paper}>
+        <Box
+          px={2}
+          pb={2}
+          display="flex"
+          alignItems="center"
+          flexGrow={1}>
+          <GroupAdd color="primary" className={classes.titleIcon}/>
+          <Typography
+            className={classes.titleText}
+            variant="h6"
+            color="primary"
+            component="p">Filtrar búsqueda
+          </Typography>
+        </Box>
+        <form onSubmit={onFilterUsers} id="filter-form">
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-names">Nombres</InputLabel>
+                <OutlinedInput
+                  id="input-names"
+                  type="text"
+                  name="names"
+                  label="Nombres"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-lastnames">Apellidos</InputLabel>
+                <OutlinedInput
+                  id="input-lastnames"
+                  type="text"
+                  name="lastnames"
+                  label="Apellidos"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-identification">Identificación (C.C)</InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  id="input-identification"
+                  type="number"
+                  name="identification"
+                  label="Identificación (C.C)"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-rol">Rol asociado</InputLabel>
+                <Select
+                  labelId="input-rol"
+                  name="rol"
+                  label="Rol asociado">
+                  <MenuItem value={"administrador"}>Administrador</MenuItem>
+                  <MenuItem value={"conductor"}>Conductor</MenuItem>
+                  <MenuItem value={"recolector"}>Recolector</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-state">Estado</InputLabel>
+                <Select
+                  labelId="input-state"
+                  name="state"
+                  label="Estado">
+                  <MenuItem value={1}>Activo</MenuItem>
+                  <MenuItem value={0}>Inactivo</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-phone">Teléfono</InputLabel>
+                <OutlinedInput
+                  id="input-phone"
+                  type="number"
+                  name="phone"
+                  label="Teléfono"/>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6} lg={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="input-email">Correo</InputLabel>
+                <OutlinedInput
+                  id="input-email"
+                  type="text"
+                  name="email"
+                  label="Correo"/>
+              </FormControl>
+            </Grid>
+            <Box px={1} width="100%">
+              <Grid container spacing={1}>
+                <Grid item xs={6}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    color="secondary"
+                    fullWidth>Filtrar
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      (document
+                        .getElementById("filter-form") as HTMLFormElement)
+                        .reset();
+                      setFilteredUsers(undefined);
+                    }}
+                    color="secondary"
+                    fullWidth>Limpiar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Grid>
+        </form>
+      </Paper>
+    </Grid>
+  </Grid>
+}
+
+export default UsersData;
---
+++ src/components/UsersData/UsersTable.tsx
@@ -0,0 +1,227 @@
+import React, { forwardRef, useContext, useState } from "react";
+import firebase from "firebase";
+import MaterialTable, { Icons } from "material-table";
+import { Snackbar, Box } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+import firebaseInitApp, { onDeleteUser, onModifyUser } from "../../services/Firebase";
+import { IUserData } from "./UsersData";
+import { AuthContext } from "../Tools/Auth";
+
+const MaterialTableIcons: Icons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} color="primary" ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+export interface IUsersTable {
+  users: IUserData[] | undefined;
+  filteredUsers: IUserData[] | undefined;
+  canEdit: boolean;
+  setUsers: (userData: IUserData[]) => void;
+}
+
+const UsersTable = ({ users, filteredUsers, canEdit, setUsers }: IUsersTable) => {
+  const [snackbar, setSnackbar] = useState<{
+    type: "success" | "info" | "warning" | "error",
+    message: string, time?: number } | null>(null);
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (users) return <Box>
+    <MaterialTable
+      style={{
+        tableLayout: "fixed",
+        whiteSpace: "nowrap",
+        boxShadow: "none",
+        MozBoxShadow: "none",
+        WebkitBoxShadow: "none",
+      }}
+      options={{
+        filtering: true,
+        actionsColumnIndex: 7,
+        exportButton: true,
+        exportFileName: "tabla_de_usuarios"
+      }}
+      icons={MaterialTableIcons}
+      columns={[
+        { title: "Nombres", field: "names" },
+        { title: "Apellidos", field: "lastnames" },
+        { title: "Identificación (C.C)", field: "identification" },
+        { title: "Rol asociado", field: "rol", lookup: {
+          "administrador": "Administrador",
+          "conductor": "Conductor",
+          "recolector":"Recolector" } },
+        { title: "Estado", field: "state", lookup: { true: "Activo", false: "Inactivo" } },
+        { title: "Teléfono", field: "phone" },
+        { title: "Correo", field: "email" }
+      ]}
+      data={filteredUsers ?? users}
+      title=""
+      localization={{
+        body: {
+          editRow: {
+            deleteText: '¿Desea eliminar este usuario?',
+            cancelTooltip: "Cancelar",
+            saveTooltip: 'Guardar',
+          },
+          editTooltip: "Editar usuario",
+          deleteTooltip: "Eliminar usuario"
+        },
+        header: {
+          actions: "Acciones",
+        }
+      }}
+      editable={canEdit ? {
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {            
+              if (isNaN(Number(newData.phone))) {
+                setSnackbar({ type:"error", message: "Teléfono invalido" });
+                reject();
+                return
+              }
+              if (isNaN(Number(newData.identification))) {
+                setSnackbar({ type:"error", message: "Identificación invalida" });
+                reject();
+                return
+              }
+  
+              const dataUpdate = [...users];
+              const index = dataUpdate.findIndex(
+                (data) => data.email === oldData!.email);
+              
+              const email = oldData!.email;
+              const newEmail = newData.email;
+              dataUpdate[index] = newData;
+  
+              if (email !== newEmail) {
+                // eslint-disable-next-line
+                const mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                const validEmail = mailRegex.test(newEmail.toLowerCase());
+
+                if (validEmail) {
+                  onModifyUser(email, newEmail).then(async (response) => {
+                    console.log(response);
+                    const userCollection = firebase.firestore(firebaseInitApp)
+                      .collection("USER");
+                    await userCollection.doc(newData.id).set({
+                      names: newData.names,
+                      lastnames: newData.lastnames,
+                      identification: Number(newData.identification),
+                      rol: newData.rol,
+                      state: newData.state,
+                      phone: Number(newData.phone),
+                      email: newData.email
+                    });
+    
+                    setUsers([...dataUpdate]);
+                    resolve();
+                  }).catch(() => {
+                    setSnackbar({ type:"error", message: "Algo ha salido mál editando el usuario" });
+                    reject();
+                    return
+                  });
+                } else {
+                  setSnackbar({ type:"error", message: "No es un correo válido" });
+                  reject();
+                  return
+                }
+              } else {
+                const userCollection = firebase.firestore(firebaseInitApp)
+                  .collection("USER");
+                userCollection.doc(newData.id).set({
+                  names: newData.names,
+                  lastnames: newData.lastnames,
+                  identification: Number(newData.identification),
+                  rol: newData.rol,
+                  state: newData.state,
+                  phone: Number(newData.phone),
+                  email: newData.email
+                }).then(() => {
+                  setUsers([...dataUpdate]);
+                  resolve()
+                }).catch(() => {
+                  setSnackbar({ type:"error", message: "Algo ha salido mál editando el usuario" });
+                  reject();
+                  return
+                });
+              }
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...users];
+              const index = dataDelete.findIndex(
+                (data) => data.email === oldData.email);
+              dataDelete.splice(index, 1);
+  
+              const email = oldData.email as string;
+  
+              if (currentUser?.email === email) {
+                setSnackbar({ type:"error", message: "No puede eliminar el usuario actual" });
+                reject();
+              } else {
+                onDeleteUser(email).then(async (response) => {
+                  console.log(response);
+                  if (currentUser?.email === email) 
+                    await firebaseInitApp.auth().signOut();
+                  const userCollection = firebase.firestore(firebaseInitApp)
+                  .collection("USER");
+                  await userCollection.doc(oldData.id).delete();
+                  setUsers([...dataDelete]);
+  
+                  resolve();
+                }).catch(() => {
+                  setSnackbar({ type:"error", message: "Algo ha salido mál eliminadno el usuario" });
+                  reject();
+                  return
+                });
+              }
+            }, 1000)
+          }),
+      } : undefined}/>
+    <Snackbar
+      open={snackbar !== null}
+      autoHideDuration={snackbar?.time ?? 6000}
+      onClose={() => setSnackbar(null)}>
+      <Alert onClose={() => setSnackbar(null)} severity={snackbar?.type}>
+        {snackbar?.message}
+      </Alert>
+    </Snackbar>
+  </Box>
+  else return null;
+}
+
+export default UsersTable;
